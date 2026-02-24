13d198accd68a6dfdfcf815c197c23613c5fbf88
VonRiddarn <vonriddarn@gmail.com>
2026-02-24 12:43:55 +0100

    feat: add basic hero module with styling and placehodler image TODO: Add CTAs and improve styling

--- src/page-components/Hero/Hero.tsx
+++ src/page-components/Hero/Hero.tsx
@@ -1,5 +1,26 @@
+import styles from "./Hero.module.scss";
+
 const Hero = () => {
-	return <div>Hero</div>;
+	return (
+		<section className={styles["hero"]}>
+			<span className={styles["hero__introduction"]}>
+				<img
+					className={styles["hero__introduction__image"]}
+					src="https://picsum.photos/200/300"
+					alt="ddd"
+					width={200}
+					height={200}
+				/>
+				<span className={styles["hero__introduction__tagline"]}>
+					<p>I'm</p>
+					<h1>Khaled Zraiqi,</h1>
+					<p className={styles["hero__introduction__tagline__gamedev"]}>
+						the <span>gamedeveloper</span>.
+					</p>
+				</span>
+			</span>
+		</section>
+	);
 };
 
 export default Hero;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,9 @@
+import Hero from "../page-components/Hero/Hero";
+
 const Home = () => {
 	return (
 		<>
-			<h1>Home</h1>
-			<p>dsadas</p>
-			<h2>Home</h2>
-			<p>dsadas</p>
-			<h3>Home</h3>
-			<p>dsadas</p>
-			<h4>Home</h4>
-			<p>dsadas</p>
+			<Hero />
 		</>
 	);
 };
