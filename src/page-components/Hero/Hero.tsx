cf4554ef82dd33223028da771296f9427b9e5541
VonRiddarn <vonriddarn@gmail.com>
2026-02-25 14:26:28 +0100

    feat: add inline icons to hero buttons and improve styling

--- src/page-components/Hero/Hero.tsx
+++ src/page-components/Hero/Hero.tsx
@@ -1,16 +1,18 @@
+import HeroIcon from "../HeroIcon/HeroIcon";
 import styles from "./Hero.module.scss";
 
 const Hero = () => {
 	return (
 		<section className={styles["hero"]}>
 			<div className={styles["hero__bg"]}></div>
-			<section className={styles["hero__introduction"]}>
+			<section className={`${styles["hero__introduction"]} noselect`}>
 				<img
 					className={styles["hero__introduction__image"]}
 					src="https://picsum.photos/200/300"
 					alt="ddd"
 					width={200}
 					height={200}
+					draggable={false}
 				/>
 				<span className={styles["hero__introduction__tagline"]}>
 					<p>I'm</p>
@@ -21,10 +23,38 @@ const Hero = () => {
 				</span>
 			</section>
 			<section className={styles["hero__socials"]}>
-				<button className={styles["hero__socials__button"]}>Github</button>
-				<button className={styles["hero__socials__button"]}>LinkedIn</button>
-				<button className={styles["hero__socials__button"]}>Itch.io</button>
-				<button className={styles["hero__socials__button"]}>Resumé</button>
+				<span className={styles["hero__socials__wrapper"]}>
+					<button className={styles["hero__socials__wrapper__button"]}>
+						<HeroIcon
+							className={styles["hero__socials__wrapper__button__icon"]}
+							icon={"github"}
+							ariaLabel={"Github profile"}
+						/>
+					</button>
+					<button className={styles["hero__socials__wrapper__button"]}>
+						<HeroIcon
+							className={styles["hero__socials__wrapper__button__icon"]}
+							icon={"linkedin"}
+							ariaLabel={"LinkedIn profile"}
+						/>
+					</button>
+				</span>
+				<span className={styles["hero__socials__wrapper"]}>
+					<button className={styles["hero__socials__wrapper__button"]}>
+						<HeroIcon
+							className={styles["hero__socials__wrapper__button__icon"]}
+							icon={"itch"}
+							ariaLabel={"Itch profile"}
+						/>
+					</button>
+					<button className={styles["hero__socials__wrapper__button"]}>
+						<HeroIcon
+							className={styles["hero__socials__wrapper__button__icon"]}
+							icon={"resume"}
+							ariaLabel={"Resume PDF"}
+						/>
+					</button>
+				</span>
 			</section>
 		</section>
 	);
